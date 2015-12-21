30f59e6462f416959fa2e3295853e1f883cdaa53
Carl Chang <allermine@gmail.com>
2015-12-22 07:01:47 +0800

    Update computing GSR index.R

--- computing GSR index.R
+++ computing GSR index.R
@@ -14,7 +14,7 @@
 
 library(combinat)
 
-disease='OvcaMucinous'    # assigning the disease name 
+disease='OvcaClearCell'    # assigning the disease name; ovarian clear cell carcinoma for the dataset 
 annotation= 'GO'          # usign GO gene set for annotation 
 file= paste('D:/working/DB/geneSet/','go.gmt', sep='')  # location of go.gmt file   
 gs.number= nrow(read.csv(file, header=F))    # gene set number 
